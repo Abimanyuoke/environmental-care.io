b16f7eba4e4a955778908e97ee4ceb2ce9ef6e22
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-07-05 11:18:22 +0700

    three commit

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector (".menu")
+const btn = document.querySelector(".fa-circle-arrow-up");
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("is-active");
 
     menu.classList.toggle("menu-active");
+});
+
+window.addEventListener('scroll', () => {
+    windowPosition = window.scrollY >= 300;
+    btn.classList.toggle("btn-active", windowPosition);
 });
